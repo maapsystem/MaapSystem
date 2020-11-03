c7a67b6d641e76bd75a7dfeca637a1580f377dab
alexaugusto23 <contato.alexaugusto@hotmail.com>
2020-11-03 16:20:59 -0300

    artefato 26 interfaces internas e prótotipo do sistema com módulo login

--- app_sorvetunes.py
+++ app_sorvetunes.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, render_template , redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from flask.ext.security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required
 import os
 
 app_sorvetunes = Flask(__name__)
@@ -9,22 +8,29 @@ db = SQLAlchemy(app_sorvetunes)
 
 class Log(db.Model):
     __tablename__ = "tb_cadastro_login"
-    usuario = db.Column(db.String(10), nullable=True, unique=True, primary_key=True)
+    ids = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    usuario = db.Column(db.String(10), nullable=True, unique=True)
     senha = db.Column(db.String(8), nullable=True)
 
     def __init__(self, usuario, senha):
         self.usuario = usuario
         self.senha = senha
 
+
 @app_sorvetunes.route("/")
-@app_sorvetunes.route("/login", methods=["PUT", "POST"])
+@app_sorvetunes.route("/login", methods=["GET","POST"])
 def login():
+    return render_template('login.html')
+
+
+@app_sorvetunes.route("/form", methods=["PUT", "POST"])
+def form():
     usuarios = Log.query.all()
-    usuario = request.form['usuario']
-    senha = request.form['senha']
+    login = request.form['usuarioform']
+    password = request.form['senhaform']
     for user in usuarios:
-        if user['usuario'] == usuario and user['senha'] == senha:
-            return render_template("index.html")
+        if user.usuario == login and user.senha == password:
+            return render_template("index.html", mensagem = "Login Realizado.")
     return render_template("login.html", mensagem = "Login inválido.")
 
 
@@ -33,11 +39,3 @@ if __name__ == '__main__':
     #db.drop_all()
     port = int(os.environ.get("PORT",5000))
     app_sorvetunes.run(debug=True, host='0.0.0.0', port=port)
-
-
-'''
-select * from tb_cadastro_login
-
-INSERT INTO tb_cadastro_login(column1, column2, …)
-VALUES (value1, value2, …);
-'''
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-usuario
+@app_sorvetunes.route("/form", methods=["PUT", "POST"])
+def form():
+    usuarios = Log.query.all()
+    print(usuarios)
+    login = request.form['usuarioform']
+    password = request.form['senhaform']
+    for user in usuarios:
+        if user['usuario'] == login and user['senha'] == password:
+            return render_template("index.html", login=login)
+    return render_template("login.html", mensagem = "Login inválido.")
