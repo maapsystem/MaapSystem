834308a02f7509983eef59dbb41b4f8b2d56ab7b
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-02-22 10:12:20 -0300

    coreeção host

--- app.py
+++ app.py
@@ -17,4 +17,4 @@ if __name__ == '__main__':
     #db.create_all()
     #db.drop_all()
     port = int(os.environ.get("PORT",5000))
-    app.run(debug=True, host='localhost', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
