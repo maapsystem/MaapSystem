4ac16bfbcfbc601d71ff6250d1525767d3ad28ff
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-04-23 20:08:22 -0300

    subindo script sql.

--- sql_selects_all_tables.sql
+++ sql_selects_all_tables.sql
@@ -15,9 +15,16 @@ SELECT * FROM tbl_produto;
 INSERT INTO tbl_login (cod_cliente, nome, senha)
 VALUES(1, 'alex', 'admin@1');
 
+UPDATE tbl_cliente
+SET cod_cidade = 1
+WHERE id_cliente = 1;
+
 SELECT *
 FROM tbl_login
-JOIN tbl_pessoa_fisica
-ON tbl_login.cod_cliente = tbl_pessoa_fisica.id_pessoa_fisica;
+JOIN tbl_pessoa_fisica ON tbl_login.cod_cliente = tbl_pessoa_fisica.id_pessoa_fisica
+JOIN tbl_cliente ON tbl_login.cod_cliente = tbl_pessoa_fisica.id_pessoa_fisica
+JOIN tbl_cidade ON tbl_cidade.id_cidade = tbl_cliente.cod_cidade
+JOIN tbl_estado ON tbl_estado.id_estado = tbl_cidade.cod_estado;
+
 
 
